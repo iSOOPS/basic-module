e32d10818e1a1fbce9d437c097e5a77a4474ef66
Samuel <>
2018-08-20 17:43:08 +0800

    新增图片下载http封装

---
+++ SServ/src/main/java/com/MainTest.java
@@ -0,0 +1,25 @@
+package com;
+
+import com.ssource.SHttp;
+
+public class MainTest {
+    public static void main(String[] args) {
+//        SHttp.getInstance().download(
+//                "http://img1.3lian.com/img013/v4/96/d/41.jpg",
+//                "/Users/samuel/Documents/Flies/aaa.jpg",
+//                new SHttp.HttpClientDownLoadProgress() {
+//
+//                    @Override
+//                    public void onProgress(int progress) {
+//                        System.out.println("download progress = " + progress);
+//                    }
+//                });
+//        SHttp.getInstance().download(
+//                "http://img1.3lian.com/img013/v4/96/d/41.jpg",
+//                "/Users/samuel/Documents/Flies/aaa.jpg");
+//        System.out.println("download end");
+//
+//        SHttp.getInstance().downloadInputStream("http://img1.3lian.com/img013/v4/96/d/41.jpg");
+    }
+
+}
--- SServ/src/main/java/com/ssource/SHttp.java
+++ SServ/src/main/java/com/ssource/SHttp.java
@@ -7,6 +7,7 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.conn.ssl.DefaultHostnameVerifier;
 import org.apache.http.conn.util.PublicSuffixMatcher;
 import org.apache.http.conn.util.PublicSuffixMatcherLoader;
@@ -16,6 +17,10 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -25,11 +30,14 @@ import java.security.NoSuchProviderException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by samuel on 2017/5/12.
  */
 public class SHttp {
+
     private RequestConfig requestConfig = RequestConfig.custom()
             .setSocketTimeout(15000)
             .setConnectTimeout(15000)
@@ -37,7 +45,9 @@ public class SHttp {
             .build();
 
     private static SHttp instance = null;
-    private SHttp(){}
+    private SHttp(){
+        downloadExcutorService = Executors.newFixedThreadPool(5);
+    }
     public static SHttp getInstance(){
         if (instance == null) {
             instance = new SHttp();
@@ -51,7 +61,8 @@ public class SHttp {
      */
     public String sendHttpPost(String httpUrl) {
         HttpPost httpPost = new HttpPost(httpUrl);// 创建httpPost
-        return sendHttpPost(httpPost);
+        httpPost.setConfig(requestConfig);
+        return basicHttpRequest(httpPost,false,true);
     }
 
     /**
@@ -69,7 +80,8 @@ public class SHttp {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return sendHttpPost(httpPost);
+        httpPost.setConfig(requestConfig);
+        return basicHttpRequest(httpPost,false,true);
     }
 
     /**
@@ -89,120 +101,58 @@ public class SHttp {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return sendHttpPost(httpPost);
+        httpPost.setConfig(requestConfig);
+        return basicHttpRequest(httpPost,false,true);
     }
 
-
-//    /**
-//     * 发送 post请求（带文件）
-//     * @param httpUrl 地址
-//     * @param maps 参数
-//     * @param fileLists 附件
-//     */
-//    public String sendHttpPost(String httpUrl, Map<String, String> maps, List<File> fileLists) {
-//        HttpPost httpPost = new HttpPost(httpUrl);// 创建httpPost
-//        MultipartEntityBuilder meBuilder = MultipartEntityBuilder.create();
-//        if (maps!=null){
-//            for (String key : maps.keySet()) {
-//                meBuilder.addPart(key, new StringBody(maps.get(key), ContentType.TEXT_PLAIN));
-//            }
-//        }
-//        for(File file : fileLists) {
-//            FileBody fileBody = new FileBody(file);
-//            meBuilder.addPart("files", fileBody);
-//        }
-//        HttpEntity reqEntity = meBuilder.build();
-//        httpPost.setEntity(reqEntity);
-//        return sendHttpPost(httpPost);
-//    }
 
     public String sendHttpPost(String httpUrl, File file) {
         String result = null;
         try {
             result = connectHttpsByPost(httpUrl, null, file);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (NoSuchProviderException e) {
-            e.printStackTrace();
-        } catch (KeyManagementException e) {
+        } catch (IOException | NoSuchAlgorithmException | KeyManagementException | NoSuchProviderException e) {
             e.printStackTrace();
         }
         return result;
     }
 
-    /**
-     * 发送Post请求
-     * @param httpPost
-     * @return
-     */
-    private String sendHttpPost(HttpPost httpPost) {
-        CloseableHttpClient httpClient = null;
-        CloseableHttpResponse response = null;
-        HttpEntity entity = null;
-        String responseContent = null;
-        try {
-            // 创建默认的httpClient实例.
-            httpClient = HttpClients.createDefault();
-            httpPost.setConfig(requestConfig);
-            // 执行请求
-            response = httpClient.execute(httpPost);
-            entity = response.getEntity();
-            responseContent = EntityUtils.toString(entity, "UTF-8");
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                // 关闭连接,释放资源
-                if (response != null) {
-                    response.close();
-                }
-                if (httpClient != null) {
-                    httpClient.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return responseContent;
-    }
+
 
     /**
      * 发送 get请求
-     * @param httpUrl
      */
     public String sendHttpGet(String httpUrl) {
         HttpGet httpGet = new HttpGet(httpUrl);// 创建get请求
-        return sendHttpGet(httpGet);
+        httpGet.setConfig(requestConfig);
+        return basicHttpRequest(httpGet,false,false);
     }
 
     /**
      * 发送 get请求Https
-     * @param httpUrl
      */
     public String sendHttpsGet(String httpUrl) {
         HttpGet httpGet = new HttpGet(httpUrl);// 创建get请求
-        return sendHttpsGet(httpGet);
+        httpGet.setConfig(requestConfig);
+        return basicHttpRequest(httpGet,true,false);
     }
 
-    /**
-     * 发送Get请求
-     * @param
-     * @return
-     */
-    private String sendHttpGet(HttpGet httpGet) {
+    private String basicHttpRequest(HttpRequestBase httpRequestBase, boolean isHttps,boolean isPost){
         CloseableHttpClient httpClient = null;
         CloseableHttpResponse response = null;
-        HttpEntity entity = null;
         String responseContent = null;
         try {
             // 创建默认的httpClient实例.
-            httpClient = HttpClients.createDefault();
-            httpGet.setConfig(requestConfig);
+            if (isHttps && !isPost){
+                PublicSuffixMatcher publicSuffixMatcher = PublicSuffixMatcherLoader.load(new URL(httpRequestBase.getURI().toString()));
+                DefaultHostnameVerifier hostnameVerifier = new DefaultHostnameVerifier(publicSuffixMatcher);
+                httpClient = HttpClients.custom().setSSLHostnameVerifier(hostnameVerifier).build();
+            }
+            else {
+                httpClient = HttpClients.createDefault();
+            }
             // 执行请求
-            response = httpClient.execute(httpGet);
-            entity = response.getEntity();
+            response = httpClient.execute(httpRequestBase);
+            HttpEntity entity = response.getEntity();
             responseContent = EntityUtils.toString(entity, "UTF-8");
         } catch (Exception e) {
             e.printStackTrace();
@@ -222,50 +172,103 @@ public class SHttp {
         return responseContent;
     }
 
-    /**
-     * 发送Get请求Https
-     * @param
-     * @return
-     */
-    private String sendHttpsGet(HttpGet httpGet) {
-        CloseableHttpClient httpClient = null;
-        CloseableHttpResponse response = null;
-        HttpEntity entity = null;
-        String responseContent = null;
+    public InputStream downloadInputStream(String url) {
+        return httpDownloadInputStream(url);
+    }
+
+    private InputStream httpDownloadInputStream(String url) {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        InputStream is = null;
         try {
-            // 创建默认的httpClient实例.
-            PublicSuffixMatcher publicSuffixMatcher = PublicSuffixMatcherLoader.load(new URL(httpGet.getURI().toString()));
-            DefaultHostnameVerifier hostnameVerifier = new DefaultHostnameVerifier(publicSuffixMatcher);
-            httpClient = HttpClients.custom().setSSLHostnameVerifier(hostnameVerifier).build();
-            httpGet.setConfig(requestConfig);
-            // 执行请求
-            response = httpClient.execute(httpGet);
-            entity = response.getEntity();
-            responseContent = EntityUtils.toString(entity, "UTF-8");
+            HttpGet httpGet = new HttpGet(url);
+            CloseableHttpResponse response = httpclient.execute(httpGet);
+            try {
+                HttpEntity httpEntity = response.getEntity();
+                is = httpEntity.getContent();
+                EntityUtils.consume(httpEntity);
+            } finally {
+                response.close();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             try {
-                // 关闭连接,释放资源
-                if (response != null) {
-                    response.close();
+                httpclient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return is;
+        }
+    }
+
+    //异步执行器
+    private ExecutorService downloadExcutorService;
+
+    public void download(final String url,
+                         final String filePath,
+                         final HttpClientDownLoadProgress progress) {
+        downloadExcutorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                httpDownloadFile(url, filePath, progress);
+            }
+        });
+    }
+
+    public void download(String url,
+                         String filePath) {
+        httpDownloadFile(url, filePath, null);
+    }
+
+    public interface HttpClientDownLoadProgress {
+        public void onProgress(int progress);
+    }
+
+    private void httpDownloadFile(String url, String filePath,
+                                  HttpClientDownLoadProgress progress) {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        try {
+            HttpGet httpGet = new HttpGet(url);
+            CloseableHttpResponse response = httpclient.execute(httpGet);
+            try {
+                HttpEntity httpEntity = response.getEntity();
+                long contentLength = httpEntity.getContentLength();
+                InputStream is = httpEntity.getContent();
+                // 根据InputStream 下载文件
+                ByteArrayOutputStream output = new ByteArrayOutputStream();
+                byte[] buffer = new byte[4096];
+                int r = 0;
+                long totalRead = 0;
+                while ((r = is.read(buffer)) > 0) {
+                    output.write(buffer, 0, r);
+                    totalRead += r;
+                    if (progress != null) {// 回调进度
+                        progress.onProgress((int) (totalRead * 100 / contentLength));
+                    }
+                    System.out.println((int) (totalRead * 100 / contentLength));
                 }
-                if (httpClient != null) {
-                    httpClient.close();
-                }
+                FileOutputStream fos = new FileOutputStream(filePath);
+                output.writeTo(fos);
+                output.flush();
+                output.close();
+                fos.close();
+                EntityUtils.consume(httpEntity);
+            } finally {
+                response.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                httpclient.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-        return responseContent;
     }
 
 
-
-
-
-
-    public static  String connectHttpsByPost(String path, String KK, File file) throws IOException, NoSuchAlgorithmException, NoSuchProviderException, KeyManagementException {
+    private String connectHttpsByPost(String path, String KK, File file) throws IOException, NoSuchAlgorithmException, NoSuchProviderException, KeyManagementException {
         URL urlObj = new URL(path);
         //连接
         HttpURLConnection con = (HttpURLConnection) urlObj.openConnection();
